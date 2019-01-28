fb3cdc6a5d22e9dd8c5bf06efe01406392b64cfc
kenny-tech <kenny4real2001@gmail.com>
2019-01-28 19:23:36 +0100

    Created database models and relationships

---
+++ pmanager/app/Task.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    //
+    protected $fillable = [
+        'name',
+        'project_id',
+        'user_id',
+        'days',
+        'hours',
+        'company_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongs('App\User');
+    }
+
+    public function project()
+    {
+        return $this->belongs('App\Project');
+    }
+
+    public function company()
+    {
+        return $this->belongs('App\Company');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
+
+}
+
